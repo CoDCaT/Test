b447d65e73d121a29728fb2f966a296bd1f079f4
CoDCaT <lebron-sg@yandex.ru>
2017-09-26 20:06:08 +0300

    Update QuizMvpPresenter.java

--- app/src/main/java/com/google/developer/bugmaster/QuizMvpPresenter.java
+++ app/src/main/java/com/google/developer/bugmaster/QuizMvpPresenter.java
@@ -3,5 +3,5 @@ package com.google.developer.bugmaster;
 import com.google.developer.bugmaster.base.MvpPresenter;
 
 public interface QuizMvpPresenter<V extends QuizMvpView> extends MvpPresenter<V> {
-      void onViewInitialized();
+      void onViewInitialized(Intent intent);
 }
