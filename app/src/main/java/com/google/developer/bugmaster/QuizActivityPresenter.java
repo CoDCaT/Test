c76a80688c916ee98a5942a0037a40e57a3c96ae
CoDCaT <lebron-sg@yandex.ru>
2017-09-26 16:24:38 +0300

    Update QuizActivityPresenter.java

--- app/src/main/java/com/google/developer/bugmaster/QuizActivityPresenter.java
+++ app/src/main/java/com/google/developer/bugmaster/QuizActivityPresenter.java
@@ -3,9 +3,14 @@ package com.google.developer.bugmaster;
 import com.google.developer.bugmaster.base.BasePresenter;
 
 
-public class QuizActivityPresenter<V extends QuizMvpView> extends BasePresenter<V> {
+public class QuizActivityPresenter<V extends QuizMvpView> extends BasePresenter<V> implements QuizMvpPresenter<V> {
 
     public QuizActivityPresenter(V mMvpView) {
         super(mMvpView);
+    }
+    
+    @Override
+    public void onViewInitialized() {
+        getMvpView().showQuestion();
     }
 }
