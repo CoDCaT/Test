e8a54c3139d55515280a00cfa51210c01856af0e
CoDCaT <lebron-sg@yandex.ru>
2017-09-26 20:06:14 +0300

    Update QuizMvpView.java

--- app/src/main/java/com/google/developer/bugmaster/QuizMvpView.java
+++ app/src/main/java/com/google/developer/bugmaster/QuizMvpView.java
@@ -3,5 +3,5 @@ package com.google.developer.bugmaster;
 import com.google.developer.bugmaster.base.MvpView;
 
 public interface QuizMvpView extends MvpView {
-    void showQuestion();
+    void showQuestion(String question, ArrayList<String> options, String scientificName);
 }
