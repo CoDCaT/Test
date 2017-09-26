751211d1f9bdee2a145d514089cff774bdfbb8c6
CoDCaT <lebron-sg@yandex.ru>
2017-09-26 20:05:21 +0300

    Update QuizActivityPresenter.java

--- app/src/main/java/com/google/developer/bugmaster/QuizActivityPresenter.java
+++ app/src/main/java/com/google/developer/bugmaster/QuizActivityPresenter.java
@@ -10,7 +10,19 @@ public class QuizActivityPresenter<V extends QuizMvpView> extends BasePresenter<
     }
     
     @Override
-    public void onViewInitialized() {
-        getMvpView().showQuestion();
+    public void onViewInitialized(Intent intent) {
+        
+        List<Insect> insects = intent.getParcelableArrayListExtra(QuizActivity.EXTRA_INSECTS);
+        Insect selected = getIntent().getParcelableExtra(QuizActivity.EXTRA_ANSWER);
+        
+        //Load answer strings
+        ArrayList<String> options = new ArrayList<>();
+        for (Insect item : insects) {
+            options.add(item.scientificName);
+        }
+        
+        String question = getString(R.string.question_text, selected.name);
+        
+        getMvpView().showQuestion(question, options, selected.getScientificName());
     }
 }
