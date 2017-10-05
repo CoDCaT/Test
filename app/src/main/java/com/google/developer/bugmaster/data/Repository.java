978645537ea5d115ea7a280c50c4bb3023a33efa
CoDCaT <lebron-sg@yandex.ru>
2017-10-05 23:36:26 +0400

    add repository

--- app/src/main/java/com/google/developer/bugmaster/data/Repository.java
+++ app/src/main/java/com/google/developer/bugmaster/data/Repository.java
@@ -1,6 +1,31 @@
 package com.google.developer.bugmaster.data;
 
+import java.util.List;
 
-public interface Repository {
 
+public class Repository implements IRepository<Insect>{
+
+    public Repository() {
+
+    }
+
+    @Override
+    public void add(Insect item) {
+
+    }
+
+    @Override
+    public void remove(Insect item) {
+
+    }
+
+    @Override
+    public void update(Insect item) {
+
+    }
+
+    @Override
+    public List<Insect> query() {
+        return null;
+    }
 }
